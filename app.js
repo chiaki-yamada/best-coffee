5a12b74040f10ef219fdee2d6d3890dc2aab2242
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-06-04 13:20:15 -0700

    updated files

---
+++ app.js
@@ -0,0 +1,7 @@
+const ham = document.querySelector('#js-hamburger');
+const nav = document.querySelector('#js-nav');
+
+ham.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  ham.classList.toggle('active');
+});
